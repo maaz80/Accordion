a28d97d067380bfbf1f574931280f9859e296727
maaz80 <maazforlap@gmail.com>
2024-04-12 08:08:32 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import data from "./components/Accodian/data"
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selected, setSelected] = useState(0)
+
+  const handlesingleselection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null :getCurrentId)
+  }
 
   return (
     <>
-      <div className='bg-red-600'>Maaz</div>
+    <Navbar/>
+      <div className='bg-red-600 flex justify-center items-center overflow-hidden'>
+        <div className='bg-orange-300 flex flex-col justify-center  p-10 rounded-md md:w-[30%] w-[90%]'>
+          {
+            data && data.length > 0 ? (
+              data.map((dataItem) => (
+                <div key={dataItem.id} className='Item'>
+                  <div onClick={() => { handlesingleselection(dataItem.id) }}>
+                    <h3 className='flex justify-between bg-gray-600 rounded-md my-3 items-center pl-2'>{dataItem.question}  <span className='flex justify-end bg-red-600 rounded-md p-2 hover:font-bold hover:bg-red-800'>+</span></h3>
+                   
+                  </div>
+                  {
+                    selected === dataItem.id ?
+                      <div className='bg-red-600 rounded-md text-center mb-3'>{dataItem.answer}</div>
+                      : null
+                  }
+                </div>
+              ))
+            ) : (
+              ""
+            )
+          }
+
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Accodian/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      "id": 1,
+      "question": "What is the capital of France?",
+      "answer": "Paris"
+    },
+    {
+      "id": 2,
+      "question": "Who wrote the novel \"1984\"?",
+      "answer": "George Orwell"
+    },
+    {
+      "id": 3,
+      "question": "What is the chemical symbol for gold?",
+      "answer": "Au"
+    },
+    {
+      "id": 4,
+      "question": "How many continents are there?",
+      "answer": "Seven"
+    },
+    {
+      "id": 5,
+      "question": "What year did World War II end?",
+      "answer": "1945"
+    }
+  ]
+  export default data;  
